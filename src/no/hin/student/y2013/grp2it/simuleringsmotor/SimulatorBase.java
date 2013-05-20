c129e93e60a4f95e34ca333a3ce99921aac66697
runar <runar@LYWPC01.lyngmo.wixtra.com>
2013-05-20 10:49:14 +0200

    .

--- src/no/hin/student/y2013/grp2it/simuleringsmotor/SimulatorBase.java
+++ src/no/hin/student/y2013/grp2it/simuleringsmotor/SimulatorBase.java
@@ -1,9 +1,26 @@
 package no.hin.student.y2013.grp2it.simuleringsmotor;
 
+import org.w3c.dom.NodeList;
+
 /*
  * Mai 2013 - Gruppe 2 IT
  */
 
 public class SimulatorBase {
+	private NodeList nodeList;
 
+	public SimulatorBase()
+	{
+		this.nodeList = null;
+	}
+	
+	public void setXMLNodeList(NodeList nodeList) {
+		  
+	  
+	}
+	  
+	public NodeList getXMLNodeList()
+	{
+		return this.nodeList;
+	}
 }
