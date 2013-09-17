4d5c2455abf10f24b229f0f4db534f111be66dd0
Jakob Treland <jakob.treland@gmail.com>
2013-09-18 00:42:55 +0200

    Ferdig med varmetap
    (forhåpentligvis)

--- src/no/hin/student/y2013/grp2it/simuleringsmotor/Varmetap.java
+++ src/no/hin/student/y2013/grp2it/simuleringsmotor/Varmetap.java
@@ -7,7 +7,7 @@ public class Varmetap extends BygningBase {
 	{
 		double temperatur = SimuleringsMotor.getKlima().getTemperatureForTime(startTime);
 		double gjenvinningsgrad = 0, kuldebro = 0, uvegg = 0, utak = 0, uvinduDør = 0, ugulv = 0, lekkasjetall = 0; //Varmetapsverdier (U-verdier)
-		
+		double varmetap = 0;
 		//Angir verdiene i henhold til byggstandard, og beregner varmetapet
 		if (this.byggstandard < 1987) {
 			utak = 0.23;
@@ -20,7 +20,7 @@ public class Varmetap extends BygningBase {
 			
 		}
 		
-		if (this.byggstandard >= 1987 && byggstandard < 1997) {
+		if (this.byggstandard >= 1987 && this.byggstandard < 1997) {
 			utak = 0.20;
 			uvegg = 0.30;
 			ugulv = 0.30;
@@ -43,17 +43,17 @@ public class Varmetap extends BygningBase {
 		//beregner varmetapet gjennom tak, vegg, gulv, vinduer og dører, kuldebro, infiltrasjon, ventilasjon
 		
 		//Resultatet er i W/K, som vil si antall watt tapt per time, og skal ganges med antall grader under 17C. 
-		this.varmetap = (yttertakAreal*utak)+(ytterveggAreal*uvegg)+(bruttoAreal*ugulv)+(vinduDørAreal*uvinduDør)+(kuldebro*bruttoAreal)+(0.33*lekkasjetall*luftVolum*0.07)+(0.33*(luftVolum*0.5)*(1-gjenvinningsgrad));
-		
+		varmetap = (this.yttertakAreal*utak)+(this.ytterveggAreal*uvegg)+(this.bruttoAreal*ugulv)+(this.vinduDørAreal*uvinduDør)+(kuldebro*this.bruttoAreal)+(0.33*lekkasjetall*this.luftVolum*0.07)+(0.33*(this.luftVolum*0.5)*(1-gjenvinningsgrad));
 		if (temperatur<17){
-			this.varmetap = this.varmetap*(17-temperatur);
+			varmetap = varmetap*(17-temperatur);
 		}
 		//Ved 17 grader og opp er det ikke noe varmetap
 		if (temperatur>=17){
-			this.varmetap = 0;
+			varmetap = 0;
 		}
-		
-		System.out.format("varmetap: %f\n\n", this.varmetap);
+
+		this.energiForbruk = varmetap;
+		System.out.format("Varmetap per time: %f\n\n", this.energiForbruk);
 		
 		return true;
 	}
