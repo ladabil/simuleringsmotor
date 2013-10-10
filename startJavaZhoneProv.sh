f9af4fe4a0f2aee07f9da9a67f0698b7ddacbac1
runar <runar@LYPC07.lyngmo.wixtra.com>
2013-10-10 22:33:19 +0200

    .

--- startJavaZhoneProv.sh
+++ startJavaZhoneProv.sh
@@ -10,6 +10,9 @@ MYCLASSPATH="${MYCLASSPATH}:${CLASSPATH}/commons-email-1.3.1.jar"
 MYCLASSPATH="${MYCLASSPATH}:${CLASSPATH}/commons-net-3.3.jar"
 #MYCLASSPATH="${MYCLASSPATH}:${CLASSPATH}/mysql-connector-java-5.1.17-bin.jar"
 MYCLASSPATH="${MYCLASSPATH}:/usr/share/java/javamail.jar"
+MYCLASSPATH="${MYCLASSPATH}:/home/gruppe2/simuleringsmotor/bin"
+
 #export JAVA_HOME="/usr/lib/jvm/jre-openjdk/"
 
-/usr/bin/jsvc -jvm server -pidfile /var/run/simuleringsMotor_${1}.pid -outfile /tmp/out_${1}.log -errfile /tmp/err_${1}_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.${1}
+/usr/bin/java -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
+#/usr/bin/jsvc -jvm server -pidfile /var/run/simuleringsMotor_${1}.pid -outfile /tmp/out_${1}.log -errfile /tmp/err_${1}_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.${1}
