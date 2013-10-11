d7842f2964a73a9ffa1cd0114d185181d03b78a3
runar <runar@LYPC07.lyngmo.wixtra.com>
2013-10-11 09:16:26 +0200

    .

--- src/no/hin/student/y2013/grp2it/simuleringsmotor/FilMonitor.java
+++ src/no/hin/student/y2013/grp2it/simuleringsmotor/FilMonitor.java
@@ -64,7 +64,7 @@ public class FilMonitor {
 //		        	System.err.printf("Filename: %s\n", tmpFile.getAbsolutePath(), tmpFile.getName());
 //		        	System.exit(2);
 		        	
-//		        	file.toFile().renameTo(tmpFile);
+		        	file.toFile().renameTo(tmpFile);
 		        }
 		    }
 		} catch (IOException | DirectoryIteratorException x) {
