738aad5d004d74f1ff2948a3d10a85174e8b6c2e
RL <ladabil@gmail.com>
2013-11-04 23:57:32 +0100

    .

--- startSimMotor.sh
+++ startSimMotor.sh
@@ -16,4 +16,4 @@ MYCLASSPATH="${MYCLASSPATH}:/home/gruppe2/simuleringsmotor/bin"
 #export JAVA_HOME="/usr/lib/jvm/jre-openjdk/"
 
 #/usr/bin/java -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
-/usr/bin/jsvc -jvm server -pidfile ~/simuleringsMotor_${1}.pid -outfile ~/out_${1}.log -errfile ~/err_${1}_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.${1}
+/usr/bin/jsvc -jvm server -pidfile ~/simuleringsMotor_SimuleringsMotor.pid -outfile ~/out_SimuleringsMotor.log -errfile ~/err_SimuleringsMotor_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
