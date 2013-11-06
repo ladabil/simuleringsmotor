ccfb6c9c930505eafc6be5802af3d30d7bda4963
RL <ladabil@gmail.com>
2013-11-06 21:40:00 +0100

    umask

--- startSimMotor.sh
+++ startSimMotor.sh
@@ -16,4 +16,4 @@ MYCLASSPATH="${MYCLASSPATH}:/home/gruppe2/simuleringsmotor/bin"
 #export JAVA_HOME="/usr/lib/jvm/jre-openjdk/"
 
 #/usr/bin/java -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
-/usr/bin/jsvc -jvm server -pidfile ~/simuleringsMotor_SimuleringsMotor.pid -outfile ~/out_SimuleringsMotor.log -errfile ~/err_SimuleringsMotor_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
+/usr/bin/jsvc -jvm server -umask 0002 -pidfile ~/simuleringsMotor_SimuleringsMotor.pid -outfile ~/out_SimuleringsMotor.log -errfile ~/err_SimuleringsMotor_err.log -cp ${MYCLASSPATH} no.hin.student.y2013.grp2it.simuleringsmotor.SimuleringsMotor
